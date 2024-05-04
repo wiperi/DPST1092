44c66ec3e7bec0ccf86305b8844aaeb9688b05a5
wiperi <solidtreepassing@gmail.com>
2024-05-04 23:31:38 +1000

    up

--- cr_thread.c
+++ cr_thread.c
@@ -5,18 +5,33 @@
  */
 #include <pthread.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
-void* thread_func(void*) {
-    printf("Thread: Hello father.\n");
+void* thread_func(void* arg) {
 
-    return NULL;
+    printf("Thread: Father told me: %s\n", (char*) arg);
+
+    int* ret = (int*) malloc(sizeof(int));
+    *ret = 666;
+    return (void*) ret;
 }
 
 int main(int argc, char* argv[], char* envp[]) {
 
     // create a thread
     pthread_t thread_id;
-    pthread_create(&thread_id, NULL, thread_func, NULL);
+
+    char* arg = "STAY HEALTY";
+    pthread_create(&thread_id, NULL, thread_func, arg);
+
+    // wait for end of thread
+    void* ret;
+    pthread_join(thread_id, &ret);
+
+    printf("Main: My son respond: %d\n", *((int*)ret));
+    free(ret);
+
 
     return 0;
 }
---
+++ t.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[], char *envp[]) {
+    void* p;
+    printf("%lu\n", sizeof(p));
+}
