0a887819a017e83c738bc507494887094ade12c3
Magician <solidtreepassing@gmail.com>
2024-05-08 14:39:28 +1000

    u

--- lab01r/my_args.c
+++ lab01r/my_args.c
@@ -1,15 +1,15 @@
 //
 // DPST1092 Lab 01 Exercise - my args
-// 
+//
 // Introduction:
 // print cmd args
-// 
+//
 // Reference
-// 
-// 
+//
+//
 // Authors:
 // Tianyang Chen (z5567323@unsw.edu.au)
-// 
+//
 // Written: 5/8/2024
 //
 #include <stdio.h>
@@ -17,7 +17,13 @@
 int main(int argc, char* argv[], char* envp[]) {
 
     printf("Program name: %s\n", argv[0]);
-    printf("There are %d arguments:\n", argc - 1);
+    
+    if (argc == 0) {
+        printf("There are no other arguments:\n", argc - 1);
+    } else {
+        printf("There are %d arguments:\n", argc - 1);
+    }
+    
     for (int i = 1; i < argc; i++) {
         printf("\tArgument %d is \"%s\"\n", i, argv[i]);
     }
