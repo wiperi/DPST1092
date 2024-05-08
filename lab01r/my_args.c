778e9d499138310b758a19492a249b6a7474c762
Magician <solidtreepassing@gmail.com>
2024-05-08 14:38:23 +1000

    u

--- lab01r/my_args.c
+++ lab01r/my_args.c
@@ -16,6 +16,7 @@
 
 int main(int argc, char* argv[], char* envp[]) {
 
+    printf("Program name: %s\n", argv[0]);
     printf("There are %d arguments:\n", argc - 1);
     for (int i = 1; i < argc; i++) {
         printf("\tArgument %d is \"%s\"\n", i, argv[i]);
