e00cef7590eb9b9e87620bde47fc49bdf664efa7
Magician <solidtreepassing@gmail.com>
2024-05-08 12:02:01 +1000

    up

--- lab01r/no_odd_lines.c
+++ lab01r/no_odd_lines.c
@@ -1,36 +1,34 @@
 //
 // DPST1092 Lab 01 Exercise - no odd lines
-// 
+//
 // Introduction:
 // print only even lines
-// 
+//
 // Reference
-// 
-// 
+//
+//
 // Authors:
 // Tianyang Chen (z5567323@unsw.edu.au)
-// 
+//
 // Written: 5/8/2024
 //
 #include <stdio.h>
+#include <string.h>
 
 #define STRLIM 2048
 
 int main(int argc, char* argv[], char* envp[]) {
 
-    int n = 1;
     char str[STRLIM];
 
     while (fgets(str, STRLIM, stdin) != NULL) {
 
-        if (n % 2 == 0) {
-            // even line
+        if (strlen(str) % 2 == 0) {
+            // even
             fputs(str, stdout);
         } else {
-            // odd line
+            // odd
             ;
         }
-
-        n++;
     }
 }
