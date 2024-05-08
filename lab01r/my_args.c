274b4bf52227b90cb51f0cdbf3354d1e6b34f67b
Magician <solidtreepassing@gmail.com>
2024-05-08 14:40:54 +1000

    up

--- lab01r/my_args.c
+++ lab01r/my_args.c
@@ -19,7 +19,7 @@ int main(int argc, char* argv[], char* envp[]) {
     printf("Program name: %s\n", argv[0]);
 
     if (argc == 1) {
-        printf("There are no other arguments:\n");
+        printf("There are no other arguments\n");
     } else {
         printf("There are %d arguments:\n", argc - 1);
     }
