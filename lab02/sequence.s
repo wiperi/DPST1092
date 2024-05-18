91d6e2edad44c922b75532e4e52886709f9e3c69
Magician <solidtreepassing@gmail.com>
2024-05-18 16:49:13 +1000

    up

--- lab02/sequence.s
+++ lab02/sequence.s
@@ -1,79 +1,3 @@
-    # Read three numbers `start`, `stop`, `step`
-    # Print the integers bwtween `start` and `stop` moving in increments of size `step`
-    #       
-    # Before starting work on this task, make sure you set your tab-width to 8!
-    # It is also suggested to indent with tabs only.
-    #       
-    # YOUR-NAME-HERE, DD/MM/YYYY
-
-    #![tabsize(8)]
-.text
-main:                                       # int main(void)
-    la      $a0,    prompt1                 # printf("Enter the starting number: ");
-    li      $v0,    4
-    syscall 
-    li      $v0,    5                       # scanf("%d", start);
-    syscall 
-    move    $s0,    $v0                     # s0 as start
-
-    la      $a0,    prompt2                 # printf("Enter the stopping number: ");
-    li      $v0,    4
-    syscall 
-    li      $v0,    5                       # scanf("%d", stop);
-    syscall 
-    move    $s1,    $v0                     # s1 as stop
-
-    la      $a0,    prompt3                 # printf("Enter the step size: ");
-    li      $v0,    4
-    syscall 
-    li      $v0,    5                       # scanf("%d", step);
-    syscall 
-    move    $s2,    $v0                     # s2 as step
-
-    # Part 1
-    bge     $s1,    $s0,        skip_loop1
-    bge     $s1,    $zero,      skip_loop1
-    bgez    $s2,    skip_loop1
-    # for loop
-    move    $t0,    $s0
-loop1:
-    bgt     $t0,    $s1,        loop1_end
-    li      $v0,    1
-    move    $a0,    $t0
-    syscall 
-    add     $t0,    $t0,        $s2         # i += step
-    j       loop1
-loop1_end:
-    # end of for loop
-skip_loop1:
-
-    # Part 2
-    ble     $s1,    $s0,        skip_loop2
-    ble     $s1,    $zero,      skip_loop2
-    blez    $s2,    skip_loop2
-    # for loop
-    move    $t0,    $s0
-loop2:
-    bgt     $t0,    $s1,        loop2_end
-
-    li      $v0,    1
-    move    $a0,    $t0
-    syscall 
-
-    li      $v0,    11
-    la      $a0,    '\n'
-    syscall 
-
-    add     $t0,    $t0,        $s2         # i += step
-    j       loop2
-loop2_end:
-    # end of for loop
-skip_loop2:
-
-end:
-    li      $v0,    0
-    jr      $ra                             # return 0
-
 .data
 prompt1:
     .asciiz"Enter the starting number: "
@@ -81,3 +5,100 @@ prompt2:
     .asciiz"Enter the stopping number: "
 prompt3:
     .asciiz"Enter the step size: "
+
+.text
+
+    # start: $t0
+    # stop: $t1
+    # step: $t2
+    # i: $t3
+main:
+
+    li      $v0,                    4                                   # printf("Enter the starting number: ");
+    la      $a0,                    prompt1
+    syscall 
+
+    li      $v0,                    5                                   # scanf("%d", &start);
+    syscall 
+    move    $t0,                    $v0
+
+    li      $v0,                    4                                   # printf("Enter the stopping number: ");
+    la      $a0,                    prompt2
+    syscall 
+
+    li      $v0,                    5                                   # scanf("%d", &stop);
+    syscall 
+    move    $t1,                    $v0
+
+    li      $v0,                    4                                   # printf("Enter the step size: ");
+    la      $a0,                    prompt3
+    syscall 
+
+    li      $v0,                    5                                   # scanf("%d", &step);
+    syscall 
+    move    $t2,                    $v0
+
+    blt     $t1,                    $t0,        if_stop_lt_start        #    if (stop < start) {
+    j       if_stop_lt_start_end
+
+if_stop_lt_start:
+    bge     $t2,                    $zero,      if_stop_lt_start_end    #   if (step < 0)
+
+for1_init:                                                              # for (int i = start; i >= stop; i += step) {
+    #     printf("%d\n", i);
+    # }
+
+    move    $t3,                    $t0
+for1_condition:
+    bgt     $t3,                    $t1,        for1_body
+    j       for1_end
+for1_body:
+    li      $v0,                    1
+    move    $a0,                    $t3
+    syscall 
+
+    li      $v0,                    11
+    la      $a0,                    '\n'
+    syscall 
+for1_iter:
+    add     $t3,                    $t3,        $t2
+    j       for1_condition
+for1_end:
+
+if_stop_lt_start_end:
+
+
+
+    bgt     $t1,                    $t0,        if_stop_gt_start        # if (stop > start) {
+    j       if_stop_gt_start_end                                        # if (step > 0)
+
+if_stop_gt_start:
+    ble     $t2,                    $zero,      if_stop_gt_start_end
+
+for2_init:                                                              # for (int i = start; i <= stop; i += step) {
+    #     printf("%d\n", i);
+    # }
+
+    move    $t3,                    $t0
+for2_condition:
+    blt     $t3,                    $t1,        for2_body
+    j       for2_end
+for2_body:
+    li      $v0,                    1
+    move    $a0,                    $t3
+    syscall 
+
+    li      $v0,                    11
+    la      $a0,                    '\n'
+    syscall 
+for2_iter:
+    add     $t3,                    $t3,        $t2
+    j       for2_condition
+for2_end:
+
+if_stop_gt_start_end:
+
+
+
+    jr      $ra                                                         # return 0
+
