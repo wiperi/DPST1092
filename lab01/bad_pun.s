3d08f2a98832bd9b6c414fb0803dd5e6e0da6a15
Magician <solidtreepassing@gmail.com>
2024-05-18 16:03:53 +1000

    start lab03

--- lab01/bad_pun.s
+++ lab01/bad_pun.s
@@ -2,6 +2,7 @@
 string:
     .asciiz"Well, this was a MIPStake!\n"
 
+.text
 main:
     la      $a0,    string                  # ... pass address of string as argument
     li      $v0,    4                       # ... 4 is printf "%s" syscall number
