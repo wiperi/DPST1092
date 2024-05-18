154cbf504d6c9039b5c03196dbdc1f817e2980e1
Magician <solidtreepassing@gmail.com>
2024-05-18 17:14:04 +1000

    up

--- lab02/tetrahedral.s
+++ lab02/tetrahedral.s
@@ -13,59 +13,59 @@ prompt:
 
 .text
 
-# how_many: $t0
-# n: $t1
-# total: $t2
-# j: $t3
-# i: $t4
+    # how_many: $t0
+    # n: $t1
+    # total: $t2
+    # j: $t3
+    # i: $t4
 main:
 
-    la      $a0,    prompt      # printf("Enter how many: ");
-    li      $v0,    4
+    la      $a0,        prompt              # printf("Enter how many: ");
+    li      $v0,        4
     syscall 
 
-    li      $v0,    5           # scanf("%d", number);
+    li      $v0,        5                   # scanf("%d", number);
     syscall 
-	move $t0, $v0
+    move    $t0,        $v0
 
-	li $t1, 1 					# n = 1;
+    li      $t1,        1                   # n = 1;
 
 while1_con:
-	ble $t1, $t0, while1_body 	# while (n <= how_many)
-	j while1_end
+    ble     $t1,        $t0,    while1_body # while (n <= how_many)
+    j       while1_end
 while1_body:
-	li $t2, 0 					# total = 0;
-	li $t3, 1 					# j = 1
+    li      $t2,        0                   # total = 0;
+    li      $t3,        1                   # j = 1
 while2_con:
-	ble $t3, $t1, while2_body 	# while (j <= n)
-	j while2_end
+    ble     $t3,        $t1,    while2_body # while (j <= n)
+    j       while2_end
 while2_body:
-	li $t4, 1 					# i = 1;
+    li      $t4,        1                   # i = 1;
 while3_con:
-	ble $t4, $t3, while3_body 	# while (i <= j)
-	j while3_end
+    ble     $t4,        $t3,    while3_body # while (i <= j)
+    j       while3_end
 while3_body:
-	add $t2, $t2, $t4 			# total = total+ i;
-	add $t4, $t4, 1 			# i = i + 1
-	j while3_con
+    add     $t2,        $t2,    $t4         # total = total+ i;
+    add     $t4,        $t4,    1           # i = i + 1
+    j       while3_con
 while3_end:
-	add $t3, $t3, 1 			# j = j + 1
-	j while2_con
+    add     $t3,        $t3,    1           # j = j + 1
+    j       while2_con
 while2_end:
-	li $v0, 1					# printf("%d\n", total);
-	move $a0, $t2
-	syscall
+    li      $v0,        1                   # printf("%d\n", total);
+    move    $a0,        $t2
+    syscall 
 
-	li $v0, 11
-	la $a0, '\n'
-	syscall
+    li      $v0,        11
+    la      $a0,        '\n'
+    syscall 
 
-	add $t1, $t1, 1 			# n = n + 1
+    add     $t1,        $t1,    1           # n = n + 1
 
-	j while1_con
+    j       while1_con
 while1_end:
 
 end:
-    li      $v0,    0
-    jr      $ra                 # return 0
+    li      $v0,        0
+    jr      $ra                             # return 0
 
