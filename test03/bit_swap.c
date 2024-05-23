7584608676c183003f2f9f1803ce65248c688ee4
Magician <solidtreepassing@gmail.com>
2024-05-24 05:36:22 +1000

    up

--- test03/bit_swap.c
+++ test03/bit_swap.c
@@ -4,27 +4,17 @@
 #include <stdint.h>
 #include <stdlib.h>
 
-// return value with pairs of bits swapped
-uint64_t bit_swap(uint64_t value) {
-    // PUT YOUR CODE HERE
-
-    // 10 01
-    // 01 01
-
-    // 0 2 4 ... 62
-    // 1 3 5 ... 63
-    uint64_t res = 0;
-    for (int i = 0; i < 64; i += 2) {
-        
-        uint64_t first_digit = 0;
-        first_digit |= ((1 << i) & value);
-
-        uint64_t sec_digit = 0;
-        sec_digit |= ((1 << (i + 1)) & value);
-
-        res |= (first_digit << 1);
-        res |= (sec_digit >> 1);
-    }
-
-    return res;
+uint64_t bit_swap(uint64_t x) {
+    // Mask for even bits: 0xAAAAAAAAAAAAAAAA (binary: 10101010...10)
+    uint64_t even_bits = x & 0xAAAAAAAAAAAAAAAA;
+    // Mask for odd bits: 0x5555555555555555 (binary: 01010101...01)
+    uint64_t odd_bits = x & 0x5555555555555555;
+    
+    // Shift even bits right by 1 to swap with odd bits
+    even_bits >>= 1;
+    // Shift odd bits left by 1 to swap with even bits
+    odd_bits <<= 1;
+    
+    // Combine the swapped bits
+    return even_bits | odd_bits;
 }
