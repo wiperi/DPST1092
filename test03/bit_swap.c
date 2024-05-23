33dbcd379b8c26ac7b5369c05f0700eb724508c4
Magician <solidtreepassing@gmail.com>
2024-05-24 05:33:35 +1000

    up

--- test03/bit_swap.c
+++ test03/bit_swap.c
@@ -8,5 +8,23 @@
 uint64_t bit_swap(uint64_t value) {
     // PUT YOUR CODE HERE
 
-    return 42;
+    // 10 01
+    // 01 01
+
+    // 0 2 4 ... 62
+    // 1 3 5 ... 63
+    uint64_t res = 0;
+    for (int i = 0; i < 64; i += 2) {
+        
+        uint64_t first_digit = 0;
+        first_digit &= ((1 << i) & value);
+
+        uint64_t sec_digit = 0;
+        sec_digit &= ((1 << (i + 1)) & value);
+
+        res |= (first_digit << 1);
+        res |= (sec_digit >> 1);
+    }
+
+    return res;
 }
