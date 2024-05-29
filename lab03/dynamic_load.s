4d5ea296020388ca4d66d48d509b5dea5b453319
Magician <solidtreepassing@gmail.com>
2024-05-29 12:43:30 +1000

    up

--- lab03/dynamic_load.s
+++ lab03/dynamic_load.s
@@ -1,6 +1,19 @@
+.data
+
+user_prompt_1:
+	.asciiz "Enter mips instructions as integers, -1 to finish:\n"
+	.asciiz "Starting executing instructions\n"
+
+user_prompt_2:
+	.asciiz "Finished executing instructions\n"
+
 .text
 
 main:
+	li $v0, 4
+	la $a0, user_prompt_1
+	syscall
+
 while_init:
 	li $t8, 0  # $t8 = i = 0
 while_cond:
@@ -21,6 +34,10 @@ while_end:
 	push $ra
 	jal command
 	pop $ra
+
+	li $v0, 4
+	la $a0, user_prompt_2
+	syscall
 
 	jr $ra
 
