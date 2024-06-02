d43d5889983447aa6f1f39a7e745091be6a6a3ed
Magician <solidtreepassing@gmail.com>
2024-06-02 14:10:18 +1000

    up

---
+++ find_peak.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(int argc, char* argv[], char* envp[]) {
+    int arr[] = {1, 3, 2, 5, 0};
+}
+
+void find_peak(int a[], int len) {
+    for (int i = 1; i < len - 1; i++) {
+    }
+}
