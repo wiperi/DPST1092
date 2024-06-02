6116ceed61a8c62f353de397bcfc5dc0c76d3a34
Magician <solidtreepassing@gmail.com>
2024-06-02 14:29:10 +1000

    up

--- test04/square.c
+++ test04/square.c
@@ -1,1 +1,21 @@
-/import/reed/A/dp1092/public_html/24T2/activities/square/files.ln/square.c
+// print a square of asterisks
+#include <stdio.h>
+
+int main(void) {
+    int x;
+
+    scanf("%d", &x);
+
+    int i = 0;
+    while (i < x) {
+        int j = 0;
+        while (j < x) {
+            printf("*");
+            j = j + 1;
+        }
+        i = i + 1;
+        printf("\n");
+    }
+
+    return 0;
+}
--- test04/square.s
+++ test04/square.s
@@ -3,13 +3,38 @@ main:
 	syscall			#
 	move	$t0, $v0
 
-	li	$a0, 42		# printf("%d\n", 42);
-	li	$v0, 1
+	# $t0 = x
+
+while_init:
+	li $t8, 0
+while_cond:
+	blt $t8, $t0, while_body
+	j while_end
+while_body:
+
+
+while2_init:
+	li $t7, 0
+while2_cond:
+	blt $t7, $t0, while2_body
+	j while2_end
+while2_body:
+	li $v0, 11
+	la $a0, '*'
+	syscall
+	addi $t7, $t7, 1
+
+	j while2_cond
+while2_end:
+	addi $t8, $t8, 1
+
+	li $v0, 11
+	la $a0, '\n'
 	syscall
 
-	li	$a0, '\n'	# printf("%c", '\n');
-	li	$v0, 11
-	syscall	
+	j while_cond
+while_end:
+
 
 end:
 	li	$v0, 0		# return 0
