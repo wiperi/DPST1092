37deb53f6b2072f0d955bee1b621e1f3e9e1f43d
Magician <solidtreepassing@gmail.com>
2024-06-02 14:18:29 +1000

    up

--- test04/min.c
+++ test04/min.c
@@ -1,1 +1,17 @@
-/import/reed/A/dp1092/public_html/24T2/activities/min/files.ln/min.c
+// print the minimum of two integers
+#include <stdio.h>
+
+int main(void) {
+    int x, y;
+
+    scanf("%d", &x);
+    scanf("%d", &y);
+
+    if (x < y) {
+        printf("%d\n", x);
+    } else {
+        printf("%d\n", y);
+    }
+
+    return 0;
+}
--- test04/min.s
+++ test04/min.s
@@ -1,16 +1,25 @@
 #  print the minimum of two integers
 main:
 	li	$v0, 5		# scanf("%d", &x);
-	syscall			#
+	syscall			# $t0 = x
 	move	$t0, $v0
 
 	li	$v0, 5		# scanf("%d", &y);
-	syscall			#
+	syscall			# $t1 = y
 	move	$t1, $v0
 
-	li	$a0, 42		# printf("%d\n", 42);
-	li	$v0, 1
+	blt $t0, $t1, x_lt_y
+	j x_gt_y
+x_lt_y:
+	li $v0, 1
+	move $a0, $t0 # print x
 	syscall
+	j if_end
+x_gt_y:
+	li $v0, 1
+	move $a0, $t1 # print y
+	syscall
+if_end:
 
 	li	$a0, '\n'	# printf("%c", '\n');
 	li	$v0, 11
