d1ef935b10cdc962938cf39fbfcf2d0ee90c49f0
Magician <solidtreepassing@gmail.com>
2024-07-01 19:12:03 +1000

    up

--- zzzzzz/actual_files/invalid_utf8_byte.c
+++ zzzzzz/actual_files/invalid_utf8_byte.c
@@ -1,3 +1,6 @@
+#include <stdio.h>
+#include <stdint.h>
+
 // Given an UTF-8 string, return the index of the first invalid byte.
 // If there are no invalid bytes, return -1.
 
@@ -6,5 +9,45 @@ int invalid_utf8_byte(char *utf8_string) {
     
     // TODO: implement this function
 
-    return 42;
+    uint8_t ch;
+    int i = 0;
+    while (utf8_string[i] != '\0')
+    {
+        ch = utf8_string[i];
+
+        // is head valid?
+        int tail = 0;
+        printf("%c %x\n", ch, ch);
+        if ((ch >> 7) == 0) {
+            ;
+        } else if ((ch >> 3) == 0b11110) {
+            tail = 3;
+        } else if ((ch >> 4) == 0b1110) {
+            tail = 2;
+        } else if ((ch >> 5) == 0b110) {
+            tail = 1;
+        } else {
+            // invalid
+            return i;
+        }
+
+        // is tail valid?
+        if (tail != 0) {
+            for (int j = 0; j < tail; j++) {
+                i++;
+                ch = utf8_string[i];
+
+                if ((ch >> 6) == 0b10) {
+                    ;
+                } else {
+                    // invalid
+                    return i;
+                }
+            }
+        }
+
+        i++;
+    }
+    
+    return -1;
 }
--- zzzzzz/actual_files/matchbox.c
+++ zzzzzz/actual_files/matchbox.c
@@ -10,9 +10,45 @@ struct packed_matchbox pack_matchbox(char *filename) {
     // TODO: complete this function!
     // You may find the definitions in matchbox.h useful.
 
+    // read the byte sequence length
+    FILE* file = fopen(filename, "r");
+    if (file == NULL) {
+        perror("fopen() error");
+        exit(1);
+    }
+
+    uint8_t first_byte = fgetc(file);
+    uint8_t second_byte = fgetc(file);
+    uint16_t seq_len = (first_byte) | (second_byte << 8);
+
+    int arr_len = 0;
+    if (seq_len % 8 == 0) {
+        arr_len = seq_len / 8;
+    } else {
+        arr_len = seq_len / 8 + 1;
+    }
+
+    // read the sequence
+    uint8_t* arr = malloc(arr_len * sizeof(uint8_t));
+
+    int ch;
+    for (int arr_i = 0; arr_i < arr_len; arr_i++) {
+
+        uint8_t pack = 0;
+        for (int j = 7; j >= 0; j--) {
+            ch = fgetc(file);
+            if (ch == '1') {
+                pack |= (1 << j);
+            } else if (ch == EOF) {
+                break;
+            }
+        }
+        arr[arr_i] = pack;
+    }
+
     struct packed_matchbox matchbox = {
-        .sequence_length = 0,
-        .packed_bytes = NULL
+        .sequence_length = seq_len,
+        .packed_bytes = arr,
     };
     
     return matchbox;
