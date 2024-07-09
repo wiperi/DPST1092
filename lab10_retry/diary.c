a01584fb463285b19223a14c0524d5bda8cd40d5
wiperi <solidtreepassing@gmail.com>
2024-07-09 19:14:29 +1000

    up

--- lab10_retry/diary.c
+++ lab10_retry/diary.c
@@ -1,10 +1,34 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 int main(int argc, char *argv[], char *envp[]) {
+
+    if (argc < 2) {
+        printf("Usage: %s <word> <word> <word>...\n", argv[0]);
+        return 0;
+    }
     
-    // print env
-    for (char **env = envp; *env != 0; env++) {
-        char *thisEnv = *env;
-        printf("%s\n", thisEnv);
+    // get ${HOME}
+    char *home = getenv("HOME");
+    printf("%s\n", home);
+
+    // open ${HOME}/.diary
+    char diary_path[strlen(home) + strlen(".diary") + 2];
+    sprintf(diary_path, "%s/%s", home, ".diary");
+
+    FILE* diary = fopen(diary_path, "a");
+    if(!diary) {
+        perror(diary_path);
+        exit(1);
+    }
+
+    // write to diary
+    for (int i = 1; i < argc; i++) {
+        if (i == argc - 1) {
+            fprintf(diary, "%s\n", argv[i]);
+        } else {
+            fprintf(diary, "%s ", argv[i]);
+        }
     }
 }
