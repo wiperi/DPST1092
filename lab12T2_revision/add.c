2a10197e3308a1070d3ee72371f648ec51de1cdf
Magician <solidtreepassing@gmail.com>
2024-07-23 17:57:43 +1000

    up

--- lab12T2_revision/add.c
+++ lab12T2_revision/add.c
@@ -7,6 +7,20 @@
 // return the MIPS opcode for add $d, $s, $t
 uint32_t make_add(uint32_t d, uint32_t s, uint32_t t) {
 
-    return 42; // REPLACE WITH YOUR CODE
+    d &= 0x1f;
+    s &= 0x1f;
+    t &= 0x1f;
+
+    uint32_t res = 0;
+
+    res |= (1 << 5);
+
+    res |= (d << 10);
+
+    res |= (t << 10 + 5);
+
+    res |= (s << 10 + 5 + 5);
+
+    return res;
 
 }
