b9e35c28ca3d9945ed44c49e9c5f462c967c3c7c
Magician <solidtreepassing@gmail.com>
2024-07-24 00:33:17 +1000

    up

--- weekly_test/test11/simple_xargs.c
+++ weekly_test/test11/simple_xargs.c
@@ -1,4 +1,21 @@
-/*
-    This file is intentionally provided (almost) empty.
-    Remove this comment and add your code.
-*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <posix/unistd.h>
+
+int main(int argc, char *argv[], char *envp[]) {
+    
+    if (argc != 2) {
+        printf("Usage: %s <command>\n", argv[0]);
+    }
+    
+    // get command from argv
+    char* command = argv[1];
+
+    // get argument from stdio
+    // excute command
+    char arg[1024];
+
+    while (fgets(arg, sizeof(arg), stdin) != NULL) {
+        // execute command with argument
+    }
+}
--- weekly_test/test11/unique_files.c
+++ weekly_test/test11/unique_files.c
@@ -6,5 +6,15 @@
 
 int main(int argc, char **argv)
 {
+    for (int i = 1; i < argc; i++)
+    {
+        char* file_path = argv[i];
+
+        struct stat st;
+        if (stat(file_path, &st)) {
+            
+        }
+    }
+    
     return EXIT_SUCCESS;
 }
